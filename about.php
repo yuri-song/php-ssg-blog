31882f489568693c8916326d65de007eb67e3ff3
yuri-song <sy120003@gmail.com>
2021-01-14 17:50:37 +0900

    1.14- php 다 생성

--- about.php
+++ about.php
@@ -1,44 +1,6 @@
-<!DOCTYPE html>
-<html lang="ko">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>송유리 블로그</title>
-
-  <link rel="stylesheet" href="common.css">
-  <link rel="stylesheet" href="about.css">
-</head>
-
-<body>
-<!-- 반응형 -->
-<meta name="viewport" content="width=device-width, user-scalable=no" />
-
-<!-- 폰트 어썸 불러오기 -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
-<!-- 폰트 어썸 끝 -->
-
-<!-- 제이쿼리 불러오기 -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<!-- 제이쿼리 끝 -->
-
-<!-- 토스트 ui 불러오기 -->
-<!-- 하이라이트 라이브러리 추가, 토스트 UI 에디터에서 사용됨 -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.4.1/highlight.min.js"></script>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.4.1/styles/default.min.css">
-
-<!-- 토스트 UI 에디터, 자바스크립트 코어 -->
-<script src="https://uicdn.toast.com/editor/latest/toastui-editor-all.min.js"></script>
-
-<!-- 코드 미러 라이브러리 추가, 토스트 UI 에디터에서 사용됨 -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.58.3/codemirror.min.css" />
-<!-- 토스트 UI 에디터, CSS 코어 -->
-<link rel="stylesheet" href="https://uicdn.toast.com/editor/latest/toastui-editor.min.css" />
-
-<!-- 토스트 UI 에디터, 신택스 하이라이트 플러그인 추가 -->
-<script src="https://uicdn.toast.com/editor-plugin-code-syntax-highlight/latest/toastui-editor-plugin-code-syntax-highlight.min.js"></script>
-<!-- 토스트 ui 끝 -->
-
+<?php
+require_once "head.php";
+?>
 
 <div class="body-content">
   <header class="visible-md-up">
@@ -132,3 +94,6 @@
         </span>
       </h1>
     </section>
+<?php
+require_once "foot.php";
+?>
