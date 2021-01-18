ca3823d7fd895d0bc53a42013f334d8d2bd3558d
yurong51 <76942943+yurong51@users.noreply.github.com>
2021-01-18 09:44:33 +0900

    1.18 - date.php 추가

--- about.ssghtml.php
+++ about.ssghtml.php
@@ -1,4 +1,5 @@
 <?php
+require_once "date.php";
 require_once "head.php";
 ?>
 
--- head.php
+++ head.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>송유리 블로그</title>
+  <title><?=$siteTitle?></title>
 
   <link rel="stylesheet" href="common.css">
   <link rel="stylesheet" href="index.css">
