128a668ae8354d72b6de16b12e384e4cfb95fc51
yurong51 <76942943+yurong51@users.noreply.github.com>
2021-01-19 01:27:40 +0900

    1. 19 - 1:27

--- article_detail.php
+++ article_detail.php
@@ -37,21 +37,23 @@
 <section class="section-article-reply-box padding-0-10 con-min-width">
     <div class="con">
         <div id="disqus_thread"></div>
-
         <script>
             var disqus_config = function () {
-                this.page.url = 'https://https://b.yrongs.com/article_detail_<?=$articleId?>.html';
-                this.page.identifier = 'article_detail_<?=$articleId?>.html';
-
+                this.page.url =
+                    https: //b.yrongs.com/article_detail_<?=$articleId?>.html';  // Replace PAGE_URL with your page's canonical URL variable
+                    this.page.identifier =
+                    'article_detail_<?=$articleId?>.html'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
             };
-            (function () { // DON'T EDIT BELOW THIS LINE 
+            */
+            (function () { // DON'T EDIT BELOW THIS LINE
                 var d = document,
                     s = d.createElement('script');
-                s.src = 'https://phpblog-2/embed.js';
+                s.src = 'https://phpblog-5.disqus.com/embed.js';
                 s.setAttribute('data-timestamp', +new Date());
                 (d.head || d.body).appendChild(s);
-
             })();
         </script>
+        <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by
+                Disqus.</a></noscript>
     </div>
 </section>
